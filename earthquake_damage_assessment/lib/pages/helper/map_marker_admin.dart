fd61132f68d0f74e4079d9a8a3af14d11c3182a6
BerkeBerktay <79627992+BerkeBerktay@users.noreply.github.com>
2023-05-27 21:32:13 +0300

    Update map_marker_admin.dart

--- earthquake_damage_assessment/lib/pages/helper/map_marker_admin.dart
+++ earthquake_damage_assessment/lib/pages/helper/map_marker_admin.dart
@@ -1,26 +1,20 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-Marker createMarker(double latitude, double longitude, int emergencyLevel,
+Marker createMarker(double latitude, double longitude, String emergencyLevel,
     String userName, String status) {
   double markerHue;
-  switch (emergencyLevel) {
-    case 1:
-      markerHue = BitmapDescriptor.hueGreen;
-      break;
-    case 2:
-      markerHue = BitmapDescriptor.hueYellow;
-      break;
-    case 3:
-      markerHue = BitmapDescriptor.hueOrange;
-      break;
-    case 4:
-      markerHue = BitmapDescriptor.hueRose;
-      break;
-    case 5:
-      markerHue = BitmapDescriptor.hueRed;
-      break;
-    default:
-      markerHue = BitmapDescriptor.hueBlue;
+  if (emergencyLevel == "1") {
+    markerHue = BitmapDescriptor.hueGreen;
+  } else if (emergencyLevel == "2") {
+    markerHue = BitmapDescriptor.hueYellow;
+  } else if (emergencyLevel == "3") {
+    markerHue = BitmapDescriptor.hueOrange;
+  } else if (emergencyLevel == "4") {
+    markerHue = BitmapDescriptor.hueRose;
+  } else if (emergencyLevel == "5") {
+    markerHue = BitmapDescriptor.hueRed;
+  } else {
+    markerHue = BitmapDescriptor.hueBlue;
   }
 
   // Create a marker
