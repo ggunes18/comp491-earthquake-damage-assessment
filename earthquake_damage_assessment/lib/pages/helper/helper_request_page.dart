20c5ad410786961d873c99ce45cb0cceec0836b9
YusufErdemirler <yerdemirler18@ku.edu.tr>
2023-06-01 01:01:26 +0300

    Edit Page Layout is fixed

--- earthquake_damage_assessment/lib/pages/helper/helper_request_page.dart
+++ earthquake_damage_assessment/lib/pages/helper/helper_request_page.dart
@@ -261,7 +261,7 @@ class _HelperRequestPageState extends State<HelperRequestPage> {
                     },
                     child: Container(
                       width: double.infinity,
-                      padding: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.all(10.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
